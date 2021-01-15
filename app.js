408a27d43c5d8d8bdb392653b448dae42e0a00a5
spbrantan <55972871+spbrantan@users.noreply.github.com>
2021-01-15 09:26:23 +0530

    First selenium actions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+const {Builder, By, Key, util} = require("selenium-webdriver")
+
+async function main () {
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get("http://google.com");
+    await driver.manage().window().maximize();
+    await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+    await driver.close();
+    await driver.quit();
+}
+
+main()
